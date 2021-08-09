7abd8c650000947fe7229f7efa5a9c3346337dcd
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2021-08-09 17:27:32 +0300

    React Components part 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import {Navbar, NavbarBrand} from "reactstrap";
 import Menu from "./components/MenuComponent";
+import {DISHES} from "./shared/dishes";
+import {Component} from "react";
 
-function App() {
-    return (
-        <div>
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-                </div>
-            </Navbar>
-            <Menu/>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes}/>
+            </div>
+        );
+    }
 }
 
 export default App;
