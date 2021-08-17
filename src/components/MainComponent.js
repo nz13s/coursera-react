adc616f0b491b7aad4dad4c33f316436e3e74c65
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2021-08-17 11:12:04 +0300

    React Router and rearranging project

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,28 +2,35 @@ import {DISHES} from "../shared/dishes";
 import {Component} from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
+import {Switch, Route, Redirect} from "react-router-dom";
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
         }
     }
 
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish});
-    }
-
     render() {
+        const HomePage = () => {
+            return(
+                <Home/>
+            )
+        }
         return (
             <div>
                 <Header/>
-                <Menu dishes={this.state.dishes} onClick={(dish) => this.onDishSelect(dish)}/>
-                <DishDetail dish={this.state.selectedDish}/>
+                <Switch>
+                    <Route path="/home" component={HomePage}/>
+                    <Route exact path="/menu"
+                           component={() => <Menu dishes={this.state.dishes}/>}
+                    />
+                    <Redirect to="/home"/>
+                </Switch>
                 <Footer/>
             </div>
         );
